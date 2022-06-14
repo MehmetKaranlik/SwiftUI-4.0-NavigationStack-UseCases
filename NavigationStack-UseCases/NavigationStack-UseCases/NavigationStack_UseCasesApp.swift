4947f2d8d0d3f89785d33f989d53abc0c6ba92d4
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-06-14 11:49:07 +0300

    Init

---
+++ NavigationStack-UseCases/NavigationStack-UseCases/NavigationStack_UseCasesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStack_UseCasesApp.swift
+//  NavigationStack-UseCases
+//
+//  Created by mehmet karanlık on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStack_UseCasesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
